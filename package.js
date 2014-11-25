42139330d6b95d4b33da6f0f4e21b8bfc88d6992
Alvaro <alvaromrinf@gmail.com>
2014-11-25 01:48:33 +0100

    Updated dependencies

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'alvaromrinf:live-form-validator',
   summary: 'Validate Semantic UI fields on live',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/avrora/live-form-validator'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
-  api.use('nooitaf:semantic-ui', 'client');
+  api.use('nooitaf:semantic-ui@1.0.0', 'client');
 
   api.addFiles(['main.js'], 'client');
   api.export('LiveFormValidator', 'client');
